e9bb02c1df7aac76d6e3dda921443542ea3fdf1a
Damaani Webber <damaani.webber@gmail.com>
2025-08-24 13:49:48 +0100

    Add star and heart broken emojis

--- niamkeltd_pylib/models/emojis.py
+++ niamkeltd_pylib/models/emojis.py
@@ -1,4 +1,6 @@
 # Emoji Code Version iOS 5, iOS 7 https://www.iemoji.com/view/emoji
+# Emoji Codepoints https://emojipedia.org/
+
 class Emojis:
     THUMBS_UP = "\U0001F44D" # 👍
     THUMBS_DOWN = "\U0001F44E" # 👎
@@ -8,5 +10,7 @@ class Emojis:
     INFORMATION = "\u2139\uFE0F" # ℹ️
     WARNING = "\u26A0\uFE0F" # ⚠️
     HEART_RED = "\u2764\uFE0F" # ❤️
+    HEART_BROKEN = "\u1F494" # 💔
     ARROW_CURVE_LEFT = "\u21A9\uFE0F" # ↩️
     FAST_FORWARD = "\u23E9" # ⏩
+    STAR = "\u2B50" # ⭐
